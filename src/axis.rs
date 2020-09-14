6e36ee5a64d99a87674b32af615d799536faa7e4
PradeepKumar <raj.pradeep.kumar@gmail.com>
2020-09-14 15:58:25 +0530

    - fixed* axis not serde

--- src/axis.rs
+++ src/axis.rs
@@ -1,4 +1,8 @@
-#[derive(Debug, PartialEq, Eq, Hash)]
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+#[derive(Debug, Hash, Ord, PartialOrd, PartialEq, Eq, Clone, Copy)]
+// #[cfg_attr(feature = "serialize", derive(Serialize, Deserialize))]
 pub enum Axis {
     XPositive,
     XNegative,
