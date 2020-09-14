1e4ca99e9ca24054e0fff493db1e8b0181983067
PradeepKumar <raj.pradeep.kumar@gmail.com>
2020-09-14 21:38:16 +0530

    - added* support for ron

--- src/config.rs
+++ src/config.rs
@@ -21,6 +21,39 @@ pub struct ConfigData
 impl InputMap{
 
     // public
+    pub fn get_bindings_as_ron(&self) ->  Result<String, String>
+    {
+        let data = ConfigData
+        {
+            keyboard_key_bindings: self.keyboard_action_binding.clone(),
+            mouse_button_binding: self.mouse_button_binding.clone(),
+            mouse_move_binding: self.mouse_move_binding.clone(),
+
+            action_deadzone: self.action_deadzone.clone()
+        };
+        let pretty = ron::ser::PrettyConfig::new()
+        .with_enumerate_arrays(true)
+        .with_new_line("\n".to_string());
+        let serialized = ron::ser::to_string_pretty(&data, pretty);
+        match serialized {
+            Ok(s) => Ok(s),
+            Err(e) => Err("Failed to generate ron".to_string()),
+        }
+    }
+
+    pub fn set_bindings_with_ron(&mut self, ron: &str)
+    {
+        let config: ConfigData = ron::de::from_str(ron)
+        .expect("Failed to deserialise config ron");
+
+        self.keyboard_action_binding = config.keyboard_key_bindings;
+        self.mouse_button_binding = config.mouse_button_binding;
+        self.mouse_move_binding = config.mouse_move_binding;
+        self.action_deadzone = config.action_deadzone;
+
+        self.action_strength_curve.clear();
+    } 
+
     pub fn get_bindings_as_json(&self) ->  Result<String, String>
     {
         let data = ConfigData
