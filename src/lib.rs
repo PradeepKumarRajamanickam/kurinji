cbb6c994030d40638df3620fe932700085482ee7
spacegiraffe <raj.pradeep.kumar@gmail.com>
2020-10-03 12:26:08 +0530

    - removed* joy setup

--- src/lib.rs
+++ src/lib.rs
@@ -9,10 +9,8 @@ pub mod actionevent;
 
 // crates
 mod action;
-mod gamepad;
 mod keyboard;
 mod mouse;
-mod serde;
 use crate::inputmap::InputMap;
 use actionevent::{OnActionActive, OnActionBegin, OnActionEnd, OnActionProgress};
 use bevy_app::prelude::*;
@@ -34,16 +32,6 @@ impl Plugin for InputMapPlugin {
             .add_system_to_stage(stage::EVENT_UPDATE, InputMap::action_event_producer.system())
             // reset
             .add_system_to_stage(stage::PRE_UPDATE, InputMap::action_reset_system.system())
-            // joystick
-            .add_system_to_stage(
-                stage::UPDATE,
-                InputMap::gamepad_connection_event_system.system(),
-            )
-            .add_system_to_stage(
-                stage::UPDATE,
-                InputMap::gamepad_button_press_input_system.system(),
-            )
-            .add_system_to_stage(stage::UPDATE, InputMap::gamepad_axis_system.system())
             // keyboard
             .add_system_to_stage(stage::UPDATE, InputMap::kb_key_press_input_system.system())
             // mouse
